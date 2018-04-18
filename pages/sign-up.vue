c0139536647b564729e1b456aa2405a4682d9540
98kNiko <1259343943@qq.com>
2018-04-18 20:57:32 +0800

    一个注册页面 4-18

---
+++ pages/sign-up.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    //修改当前的默认模板为sign.vue
+    //如果不写的话，默认是default.vue
+    layout:'sign',
+    //设置页面标题以及描述，关键字
+    head:{
+      title:'注册-简书',
+      meta:[
+        {charset:'utf-8'},
+        {name:'keywords',content:'注册页面'},
+        {name:'description',count:'注册简书'},
+        {name:'autor',content:'包瑞'}
+
+      ]
+    },
+    data(){
+      return{
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
