a35d8156683f42add68fd78e1160c897381ae141
98kNiko <1259343943@qq.com>
2018-04-23 15:51:28 +0800

    热门专题100%

--- components/myHeader.vue
+++ components/myHeader.vue
@@ -117,7 +117,6 @@
               </li>
             </ul>
           </transition>
-
         </div>
       </div>
     </nav>
@@ -136,202 +135,195 @@
   }
 </script>
 <style scoped>
-  nav{
-    width: 100%;
-    height: 56px;
+  nav {
+    width:100%;
+    height:56px;
   }
-  .navbar{
-    margin-bottom: 20px;
+  .navbar {
+    margin-bottom:20px;
     border-bottom:1px solid #f0f0f0;
-    background-color: #fff
+    background-color:#fff;
   }
-  .navbar::before{
-    content: '';
-    display: table;
+  .navbar:before {
+    content:'';
+    display:table;
   }
-  .navbar-fixed-top{
-    position: fixed;
-    left: 0;
-    top: 0;
+  .navbar-fixed-top {
+    position:fixed;
+    left:0;
+    top:0;
     z-index:9999;
   }
-  nav .width-limit{
-    min-width: 768px;
-    max-width: 1440px;
-    margin: 0 auto;
+  nav .width-limit {
+    min-width:768px;
+    max-width:1440px;
+    margin:0 auto;
   }
-  nav .logo{
-    float: left;
-    height: 56px;
-    display: block;
+  nav .logo {
+    float:left;
+    height:56px;
+    display:block;
   }
-  nav .logo img{
-    height: 100%;
+  nav .logo img {
+    height:100%;
   }
-  nav .write-btn{
-    float: right;
-    width: 100px;
-    height: 40px;
-    line-height: 40px;
-    margin: 8px 15px 0;
+  nav .write-btn {
+    float:right;
+    width:100px;
+    height:40px;
+    line-height:40px;
+    margin:8px 15px 0;
     border-radius: 20px;
-    font-size: 15px;
-    color: #fff;
-    background: #ea6f5a;
-    text-align: center;
+    font-size:15px;
+    color:#fff;
+    background:#ea6f5a;
+    text-align:center;
   }
-  nav .write-btn:hover{
-    background: rgb(236,74,93);
+  nav .wirte-btn:hover {
+    background:#ec6149;
   }
-  nav .user{
-    position: relative;
-    float: right;
+  nav .user {
+    position:relative;
+    float:right;
   }
-  nav .user .avatar{
-    position: relative;
-    display: block;
-    width: 40px;
-    height: 40px;
-    margin: 8px 28px 8px 20px;
+  nav .user .avatar {
+    position:relative;
+    display:block;
+    width:40px;
+    height:40px;
+    margin:8px 28px 8px 20px;
   }
-  nav .user .avatar:before{
+  nav .user:hover {
+    background:#f5f5f5;
+  }
+  nav .user .avatar:before {
     content:'';
-    position: absolute;
-    border-left: 5px solid transparent;
-    border-right: 5px solid transparent;
-    border-top: 5px solid #999;
+    position:absolute;
+    border-left:5px solid transparent;
+    border-right:5px solid transparent;
+    border-top:5px solid #999;
     top:18px;
-    right: -14px;
+    right:-14px;
   }
-  nav .user .avatar img{
-    width: 100%;
-    height: 100%;
+  nav .user .avatar img {
+    width:100%;
+    height:100%;
     border-radius: 50%;
-    border: 1px solid #eee;
+    border:1px solid #eee;
   }
-  nav .user:hover{
-    background-color: #f5f5f5;
-  }
-  nav .user .drop-menu{
-    position: absolute;
-    left: 0;
+  nav .user .drop-menu {
+    position:absolute;
+    left:0;
     box-shadow: 0 0 8px rgba(0,0,0,.1);
-    min-width: 160px;
-    z-index: 999;
-    margin: 0;
-    padding: 10px 0;
-    font-size: 15px;
+    min-width:160px;
+    z-index:999;
+    margin:0;
+    padding:5px 0;
+    font-size:15px;
   }
   nav .user .drop-menu li a{
-    padding: 10px 20px;
-    line-height: 30px;
-    display: block;
+    padding:10px 20px;
+    line-height:30px;
+    display:block;
   }
-  nav .user .drop-menu li a:hove{
-    background: #f5f5f5;
+  nav .user .drop-menu li a:hover {
+    background:#f5f5f5;
   }
-  nav .user .drop-menu li a i{
-    margin-right: 10px;
-    color: #ea6f5a;
-    width: 20px;
-    height: 20;
-    font-size: 20px;
-    display: inline-block;
+  nav .user .drop-menu li a i {
+    margin-right:10px;
+    color:#ea6f5a;
+    width:20px;
+    height:20px;
+    font-size:20px;
+    display:inline-block;
   }
-  nav .nav-list{
-    margin: 0;
-    transition: height 0.5s;
+  nav .nav-list {
+    margin:0;
   }
-
-  nav .nav-list:after{
-    content: "";
-    height: 0;
-    display: block;
+  nav .nav-list:after {
+    content:"";
+    height:0;
+    display:block;
     visibility: hidden;
-    clear: both;
+    clear:both;
   }
-  nav .nav-list>li{
-    float: left;
-    margin-right: 5px;
+  nav .nav-list>li {
+    margin-right:10px;
   }
-  nav .nav-list>li a{
-    display: block;
-    height: 56px;
-    line-height: 26px;
-    padding: 15px;
+  nav .nav-list>li a {
+    display:block;
+    height:56px;
+    line-height:26px;
+    padding:15px;
   }
-  nav .nav-list>li a.active{
-    color: #ea6f5a;
+  nav .nav-list>li a.active {
+    color:#ea6f5a;
     background:none!important;
   }
-  nav .nav-list>li a:hover{
-    background: #f5f5f5;
+  nav .nav-list>li a:hover {
+    background:#f5f5f5;
   }
-  nav .nav-list>li a i{
-    margin-right: 5px;
-    font-size: 20px;
+  nav .nav-list>li a i {
+    margin-right:5px;
+    font-size:20px;
   }
-  nav .nav-list .search{
-    padding-left: 15px;
-    margin-right: 10px;
+  nav .nav-list .search {
+    padding-left:15px;
+    margin-right:10px;
   }
-
-  nav  .nav-list .search form{
-    margin-bottom: 20px;
-    position: relative;
-    top: 10px;
+  nav .nav-list .search form {
+    position:relative;
+    top:9px;
+    margin-bottom:20px;
   }
-  nav .nav-list .search form .search-input{
-    width: 240px;
-    height: 38px;
-    font-size: 14px;
-    padding: 0 40px 0 20px;
-    border: 1px solid #eee;
+  nav .nav-list .search form .search-input {
+    width:240px;
+    height:38px;
+    font-size:14px;
+    padding:0 40px 0 20px;
+    border:1px solid #eee;
     background:#eee;
     border-radius: 40px;
     transition: width .5s;
   }
-  nav .nav-list .search form .search-input:focus{
-    width: 320px;
+  nav .nav-list .search form .search-input:focus {
+    width:320px;
   }
-  nav .nav-list .search form .search-input:focus~.search-btn{
-    background-color: #969696;
-    color: #fff;
+  nav .nav-list .search form .search-input:focus ~ .search-btn {
+    background:#969696;
+    color:#fff;
     border-radius: 50%;
-}
-
-  nav .nav-list .search form .search-btn{
-    position: absolute;
-    width: 30px;
-    height: 30px;
-    top: 4px;
-    right: 5px;
-    color: #969696;
-    text-align: center;
-    padding: 0;
   }
-  nav .nav-list .search form .search-btn:hover{
-    background: none;
+  nav .nav-list .search form .search-btn {
+    position:absolute;
+    width:30px;
+    height:30px;
+    top:4px;
+    right:5px;
+    color:#969696;
+    text-align:center;
+    padding:0;
   }
-
-  nav .nav-list .search form .search-btn i{
-  display: block!important;
-  margin: 0;
-  text-align: center;
-  line-height: 30px;
-  font-size: 15px;
-}
-  @media (max-width: 1440px) {
-    nav .nav-list>li a i{
-     display: none;
+  nav .nav-list .search form .search-btn:hover {
+    background:none;
+  }
+  nav .nav-list .search form .search-btn i {
+    display:block!important;
+    margin:0;
+    text-align:center;
+    line-height:30px;
+    font-size:15px;
+  }
+  @media (max-width:1440px) {
+    nav .nav-list>li a i {
+      display:none;
     }
-    nav .nav-list .search form .search-input{
-      width: 160px;
+    nav .nav-list .search form .search-input {
+      width:160px;
     }
-    nav .nav-list .search form .search-input:focus{
-      width: 240px;
+    nav .nav-list .search form .search-input:focus {
+      width:240px;
     }
-
   }
   @media (max-width:1080px) {
     nav .nav-list .search form .search-input,nav .nav-list .search form .search-input:focus {
@@ -430,7 +422,7 @@
     cursor: pointer;
     background:#eee;
   }
-
+</style>
 </style>
 
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,47 @@
 <template>
   <div style="height: 100%">
     <my-header></my-header>
-    <p>我的主体内容部分</p>
+    <div class="container index">
+      <div class="row">
+        <div class="col-xs-16 main">
+          <!--热门专题-->
+          <div class="recommend-collection">
+            <nuxt-link class="collection" to="/collection/123">
+              <img src="~/assets/img/c_1.jpg">
+              <span name="name">故事</span>
+            </nuxt-link>
+            <nuxt-link class="collection" to="/collection/123">
+              <img src="~/assets/img/c_1.jpg">
+              <span name="name">摄影</span>
+            </nuxt-link>
+            <nuxt-link class="collection" to="/collection/123">
+              <img src="~/assets/img/c_2.jpg">
+              <span name="name">人文社科</span>
+            </nuxt-link>
+            <nuxt-link class="collection" to="/collection/123">
+              <img src="~/assets/img/c_2.jpg">
+              <span name="name">历史</span>
+            </nuxt-link>
+            <nuxt-link class="collection" to="/collection/123">
+              <img src="~/assets/img/c_3.jpg">
+              <span name="name">读书</span>
+            </nuxt-link>
+            <nuxt-link class="more-collection" to="/collection/123">
+              更多热门专题
+              <i class="fa fa-angle-right"></i>
+            </nuxt-link>
+          </div>
+          <div class="split-line"></div>
+        </div>
+        <div class="col-xs-offset-1 col-xs-7 aside">
+          侧边栏
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-  import myHeader from '../components/myHeader.vue'
+  import myHeader from '../components/myHeader'
   export default {
     head:{
       title:'简书-创作你的创作',
@@ -26,6 +62,3 @@
     }
   }
 </script>
-<style>
-
-</style>
